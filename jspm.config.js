d8ea728b2690ca04de6a0e3e601fdfcd36f8550a
Thomas Girard <geek@thomasgirard.fr>
2016-03-01 12:41:45 +0100

    Update jspm config with polyfills

--- jspm.config.js
+++ jspm.config.js
@@ -1,18 +1,45 @@
 SystemJS.config({
   packageConfigPaths: [
     "npm:@*/*.json",
-    "npm:*.json"
+    "npm:*.json",
+    "github:*/*.json"
   ],
-  globalEvaluationScope: false,
   transpiler: "plugin-babel",
 
   map: {
-    "plugin-babel": "npm:systemjs-plugin-babel@0.0.2"
+    "es6-symbol": "npm:es6-symbol@3.0.2",
+    "plugin-babel": "npm:systemjs-plugin-babel@0.0.2",
+    "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+    "weakmap": "npm:weakmap@0.0.6"
   },
 
   packages: {
     "uxon": {
       "main": "main.js"
+    },
+    "npm:d@0.1.1": {
+      "map": {
+        "es5-ext": "npm:es5-ext@0.10.11"
+      }
+    },
+    "npm:es5-ext@0.10.11": {
+      "map": {
+        "es6-iterator": "npm:es6-iterator@2.0.0",
+        "es6-symbol": "npm:es6-symbol@3.0.2"
+      }
+    },
+    "npm:es6-iterator@2.0.0": {
+      "map": {
+        "d": "npm:d@0.1.1",
+        "es5-ext": "npm:es5-ext@0.10.11",
+        "es6-symbol": "npm:es6-symbol@3.0.2"
+      }
+    },
+    "npm:es6-symbol@3.0.2": {
+      "map": {
+        "d": "npm:d@0.1.1",
+        "es5-ext": "npm:es5-ext@0.10.11"
+      }
     }
   }
 });
