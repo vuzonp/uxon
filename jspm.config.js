c5018218f7efafb4cbb52dd5ca568c0893f68dc4
Thomas Girard <geek@thomasgirard.fr>
2016-04-16 19:08:58 +0200

    Updates of dependencies and removal of polyfill

--- jspm.config.js
+++ jspm.config.js
@@ -1,20 +1,29 @@
 SystemJS.config({
+  transpiler: "plugin-babel",
+  packages: {
+    "uxon": {
+      "main": "main.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-babel"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
   packageConfigPaths: [
     "npm:@*/*.json",
     "npm:*.json",
     "github:*/*.json"
   ],
-  transpiler: "plugin-babel",
-
   map: {
-    "plugin-babel": "npm:systemjs-plugin-babel@0.0.2",
-    "process": "github:jspm/nodelibs-process@0.2.0-alpha",
-    "weakmap": "npm:weakmap@0.0.6"
+    "core-js": "npm:core-js@1.2.6",
+    "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
+    "path": "github:jspm/nodelibs-path@0.2.0-alpha",
+    "plugin-babel": "npm:systemjs-plugin-babel@0.0.8",
+    "process": "github:jspm/nodelibs-process@0.2.0-alpha"
   },
-
-  packages: {
-    "uxon": {
-      "main": "main.js"
-    }
-  }
+  packages: {}
 });
